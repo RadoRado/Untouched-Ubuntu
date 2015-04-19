9ffd686d05e39ca043d0be5fda014733c33cbfe4
RadoRado <radorado@hackbulgaria.com>
2015-04-19 18:42:47 +0300

    Add link for py command

--- devtools.sh
+++ devtools.sh
@@ -5,6 +5,7 @@ sudo apt-get install vim
 sudo apt-get install git
 sudo apt-get install htop
 sudo apt-get install curl
+sudo apt-get install tilda
 
 echo "Make vim default git message editor"
 git config --global core.editor "vim"
@@ -14,3 +15,6 @@ sudo apt-get install build-essential
 sudo apt-get install libsqlite3-dev
 sudo apt-get install sqlite3
 sudo apt-get install bzip2 libbz2-dev
+
+# python or python3 is way too long to type
+sudo ln -s /usr/bin/python3 /usr/bin/py
